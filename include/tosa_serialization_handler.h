9a293aa4be98947c5d6fdb85798c6181e09b8b50
Eric Kunze <eric.kunze@arm.com>
2022-06-13 14:36:38 -0700

    Update serializer version number
    Signed-off-by: Eric Kunze <eric.kunze@arm.com>
    Change-Id: Id8ada4e2f0bcd2048c8bb9d0a20909c674bb637b

--- include/tosa_serialization_handler.h
+++ include/tosa_serialization_handler.h
@@ -28,7 +28,7 @@
 
 // Keep version number in sync with the version default value with schema/tosa.fbs
 #define TOSA_VERSION_MAJOR 0
-#define TOSA_VERSION_MINOR 24
+#define TOSA_VERSION_MINOR 25
 #define TOSA_VERSION_PATCH 0
 #define TOSA_VERSION_DRAFT true
 #define TENSOR_BUFFER_FORCE_ALIGNMENT 8
