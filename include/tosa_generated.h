fb90dd4f9e884715095674001d832321362f3e21
Eric Kunze <eric.kunze@arm.com>
2023-03-08 13:07:07 -0800

    Update version number to 0.60 for release
    Signed-off-by: Eric Kunze <eric.kunze@arm.com>
    Change-Id: I2eb93e6e88ee358c9ec9562c3b4d876eb41cc38c

--- include/tosa_generated.h
+++ include/tosa_generated.h
@@ -2123,13 +2123,13 @@ struct Version FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
     return GetField<int32_t>(VT__MAJOR, 0);
   }
   int32_t _minor() const {
-    return GetField<int32_t>(VT__MINOR, 51);
+    return GetField<int32_t>(VT__MINOR, 60);
   }
   int32_t _patch() const {
     return GetField<int32_t>(VT__PATCH, 0);
   }
   bool _draft() const {
-    return GetField<uint8_t>(VT__DRAFT, 1) != 0;
+    return GetField<uint8_t>(VT__DRAFT, 0) != 0;
   }
   bool Verify(flatbuffers::Verifier &verifier) const {
     return VerifyTableStart(verifier) &&
@@ -2149,13 +2149,13 @@ struct VersionBuilder {
     fbb_.AddElement<int32_t>(Version::VT__MAJOR, _major, 0);
   }
   void add__minor(int32_t _minor) {
-    fbb_.AddElement<int32_t>(Version::VT__MINOR, _minor, 51);
+    fbb_.AddElement<int32_t>(Version::VT__MINOR, _minor, 60);
   }
   void add__patch(int32_t _patch) {
     fbb_.AddElement<int32_t>(Version::VT__PATCH, _patch, 0);
   }
   void add__draft(bool _draft) {
-    fbb_.AddElement<uint8_t>(Version::VT__DRAFT, static_cast<uint8_t>(_draft), 1);
+    fbb_.AddElement<uint8_t>(Version::VT__DRAFT, static_cast<uint8_t>(_draft), 0);
   }
   explicit VersionBuilder(flatbuffers::FlatBufferBuilder &_fbb)
         : fbb_(_fbb) {
@@ -2171,9 +2171,9 @@ struct VersionBuilder {
 inline flatbuffers::Offset<Version> CreateVersion(
     flatbuffers::FlatBufferBuilder &_fbb,
     int32_t _major = 0,
-    int32_t _minor = 51,
+    int32_t _minor = 60,
     int32_t _patch = 0,
-    bool _draft = true) {
+    bool _draft = false) {
   VersionBuilder builder_(_fbb);
   builder_.add__patch(_patch);
   builder_.add__minor(_minor);
