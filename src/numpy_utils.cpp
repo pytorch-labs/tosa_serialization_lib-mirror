af8ebf9b8f25d1ebadbf27c26c21ca72943dd4df
Davide Grohmann <davide.grohmann@arm.com>
2024-09-27 10:34:20 +0200

    Fix compilation errors when building on Windows
    Redefine strtok_r as strtok_s on Windows builds
    
    Signed-off-by: Davide Grohmann <davide.grohmann@arm.com>
    Change-Id: Ia9110908074f21a8aac5cba717ab550ca66b3b61

--- src/numpy_utils.cpp
+++ src/numpy_utils.cpp
@@ -18,6 +18,10 @@
 #include <algorithm>
 #include <memory>
 
+#if defined(_WIN32)
+#define strtok_r strtok_s
+#endif
+
 // Magic NUMPY header
 static const char NUMPY_HEADER_STR[] = "\x93NUMPY\x1\x0\x76\x0{";
 static const int NUMPY_HEADER_SZ     = 128;
